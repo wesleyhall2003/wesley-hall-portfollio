e5df815bd410ce2c6108b9b466ccdce1fbb28b55
WESLEY HALL <hallwe@wsd.ad>
2019-06-28 09:21:21 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
